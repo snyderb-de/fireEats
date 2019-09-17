706e9b924b2e121f22de908a70c4ed7205e4cc7c
Bryan Snyder <snyderb.de@gmail.com>
2019-09-16 21:44:34 -0400

    add store page started

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,3 +2,7 @@ exports.homePage = (req, res) => {
     console.log(req.name);
     res.render('layout');
 };
+
+exports.addStore = (req, res) => {
+    res.render('editStore', { title: 'Add Store' });
+};
