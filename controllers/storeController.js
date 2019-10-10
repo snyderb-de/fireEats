3b1f95dac3605e4a6b288f6af366ecdba979ab43
Bryan Snyder <snyderb.de@gmail.com>
2019-10-09 22:37:21 -0400

    updated add store and error handling wrap

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,7 @@
+const mongoose = require('mongoose');
+
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
     console.log(req.name);
     res.render('layout');
@@ -5,4 +9,10 @@ exports.homePage = (req, res) => {
 
 exports.addStore = (req, res) => {
     res.render('editStore', { title: 'Add Store' });
+};
+
+exports.createStore = async (req, res) => {
+    const store = new Store(req.body);
+    await store.save();
+    res.redirect('/');
 };
