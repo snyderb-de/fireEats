2ecb109fb4c6ce7fb0162448a4bb102cb9d69510
Bryan Snyder <snyderb.de@gmail.com>
2019-10-18 15:07:48 -0400

    general updates

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,7 @@ const storeController = require('../controllers/storeController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', (req, res) => {
-    const wes = { name: 'Wes', age: 100, cool: false };
-    // res.send(req.query.name);
-    // res.json(req.query);
-    res.render('hello', {
-        name: 'bababooey',
-        dog: req.query.dog,
-    });
-});
-
-router.get('/reverse/:name', (req, res) => {
-    const reverse = [...req.params.name].reverse().join('');
-    res.send(reverse);
+    res.render('index');
 });
 
 /*
